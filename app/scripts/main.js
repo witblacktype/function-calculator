792cc397052af285f06c8345f225bcdff27414f0
Bretton Raymond <bretton.raymond@gmail.com>
2015-11-16 00:17:55 -0800

    Edit .jshintrc to include mocha test framework. Add header to index.html. Begin javascript development for internal api.

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,1 +1,14 @@
-console.log('\'Allo \'Allo!');
+var evaluate = {
+  add: function(input1, input2){
+
+  },
+  subtract: function(){
+
+  },
+  multiply: function(){
+
+  },
+  divide: function(){
+
+  }
+};
