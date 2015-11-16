fb09abb71cc027e76a9cb540b9af6b4cfb768922
Bretton Raymond <bretton.raymond@gmail.com>
2015-11-16 02:21:49 -0800

    Build base functionality in main.js for the API.

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,14 +1,27 @@
+'use strict';
+/*
+  functionArray used for testing purposes
+*/
+var functionArray = [];
 var evaluate = {
   add: function(input1, input2){
-
+    return input1 + input2;
   },
-  subtract: function(){
-
+  subtract: function(input1, input2){
+    return input1 - input2;
   },
-  multiply: function(){
-
+  multiply: function(input1, input2){
+    return input1 * input2;
   },
-  divide: function(){
-
+  divide: function(input1, input2){
+    return input1 / input2;
+  },
+  includeFunction: function(anyArray, anyFunction){
+    var arr = anyArray;
+    var fn = anyFunction;
+    arr.push(fn);
+    functionArray = arr;
+    return arr;
   }
 };
+console.log('module test result: ' + evaluate.add(2,4));
